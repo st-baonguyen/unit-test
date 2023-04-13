1629b0376d491f8f634ae8543cdb3b55862046dc
st-bao.nguyen <bao.nguyen@supremetech.vn>
2023-04-13 09:14:12 +0700

    change test case

--- src/app/utils/array.ts
+++ src/app/utils/array.ts
@@ -1,6 +1,10 @@
 export const isArraySorted = (array) => {
+  if (!Array.isArray(array)) {
+    return false;
+  }
   const tempArray = [...array];
   return (
+    array.length >= 1 &&
     array.every((num) => typeof num === 'number') &&
     JSON.stringify(array) === JSON.stringify(tempArray.sort((a, b) => a - b))
   );
